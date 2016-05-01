7e3d15a177512aea48bf7912c87badd7d97d3228
Watee Wichiennit <richcodeco@gmail.com>
2016-05-01 23:54:11 +0700

    Fix calendar bugs

--- backend/controllers/userCtrlServer.js
+++ backend/controllers/userCtrlServer.js
@@ -914,6 +914,7 @@ exports.getEvents = function (db) {
         }
 
         if (req.user.roles.indexOf('teacher') > -1) isInvitee = true;
+        
         if(start < today || isDecline || !isInvitee)
         {
           console.log(start < today, isDecline, !isInvitee);
