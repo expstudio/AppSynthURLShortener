b0f95cca24f7f0c9ffa73d2c4f3cb95251869d00
Watee Wichiennit <richcodeco@gmail.com>
2016-08-24 18:35:22 +0700

    Update ionic io push token

--- backend/services/pushNotification.js
+++ backend/services/pushNotification.js
@@ -5,9 +5,10 @@ exports.send = function (arrOfTokens, notification) {
     // Define relevant info
     // var jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwOWY5MGFlMi1lM2YzLTQ2NTUtYTMyOC1hYzhjMDFkMDcwM2IifQ.GC7bZGUGeAdBhOiBVFu0Zy4t_dHpPjxlWjxNb2bFXFg';
     // var IonicApiToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjOGRhMTJmYy01ZGU2LTQzMDAtODc1Yy0zZTE1ZGU4MTU4Y2QifQ.n4JFDmtmeUu0I02aF2dyDcnwnkfZ-osNePoKuauuHek";
-    var IonicApiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjOGRhMTJmYy01ZGU2LTQzMDAtODc1Yy0zZTE1ZGU4MTU4Y2QifQ.kGl7-HeTuWrMdjAHErfnToprFjErjGxoE9R2XwLZxWo";
+    // var IonicApiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjOGRhMTJmYy01ZGU2LTQzMDAtODc1Yy0zZTE1ZGU4MTU4Y2QifQ.kGl7-HeTuWrMdjAHErfnToprFjErjGxoE9R2XwLZxWo";
+    var IonicApiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjYzMyYjllNi1kZmU0LTRmNTctYjFhOS0wMmFmMGU3ZDkwZTUifQ.O6F9rYcmJjj5D39ZS3TDOidE8hRUYl2gmlWj2tyIasQ";
     var tokens = arrOfTokens;
-    var profile = 'tinyapp_production';
+    var profile = 'tinyappproduction';
     // var profile = 'tinyapp_development';
     tokens = _.filter(tokens, function(item){
       return item != null && item != "";
