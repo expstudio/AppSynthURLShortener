168fb9140eb13d32d9d77bbd7f5059fd990a42d0
Watee Wichiennit <way@iwa.fi>
2016-12-01 11:32:30 +0700

    Update get status history

--- backend/controllers/teacherCtrlServer.js
+++ backend/controllers/teacherCtrlServer.js
@@ -56,23 +56,26 @@ exports.getStatusReport = function (db) {
     return function (req, res) {
         var groupID = req.user.groupID[0];
         var query = {groupID: groupID};
-        var query_date = null;
-        if (req.query.start == req.query.end) {
-            query_date = req.query.start;
-        } else {
-            query_date = req.query.date;
-        }
 
-        if (query_date) {
-            query.date = query_date;
+        if (req.query.date) {
+            query.date = req.query.date;
         } else {
             var start = req.query.start,
                 end = req.query.end;
+
             if (start !== undefined && end !== undefined && start < end) {
                 query = {groupID: groupID, date: {$gte: start, $lte: end}};
-            };
+            } else if (start !== undefined && end !== undefined && start == end) {
+                start = new Date(req.query.start.substr(0,10)),
+                end = new Date(req.query.end.substr(0,10));
+                start.setUTCHours(0,0,0,0);
+                
+                console.log(start, end);
+            
+                query = {groupID: groupID, date: {$gte: start.toISOString(), $lte: end.toISOString()}};
+            }
         }
-
+        console.log(query);
         db.collection('historyRecords').find(query).toArray(function(err, doc) {
             if (err) {
                 throw err;
